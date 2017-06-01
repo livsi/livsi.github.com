27c531322262eb7eddf847658f063dcca55782f5
livsi <v.lezhnev@gmail.com>
2017-06-01 19:18:59 +0300

    kubernates

--- gulpfile.js
+++ gulpfile.js
@@ -23,7 +23,7 @@ var jekyllCommand = (/^win/.test(process.platform)) ? 'jekyll.bat' : 'jekyll';
  */
 gulp.task('jekyll-build', function (done) {
 	browserSync.notify(messages.jekyllBuild);
-	return cp.spawn(jekyllCommand, ['build'], {stdio: 'inherit'})
+	return cp.spawn(jekyllCommand, ['build','--incremental'], {stdio: 'inherit'})
 		.on('close', done);
 });
 
